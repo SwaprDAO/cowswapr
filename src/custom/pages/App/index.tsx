a6a24a8e61f6106389a9a94588de0eddf8a622e7
Adam Azad <adam@adamazad.com>
2022-03-06 22:26:57 +0300

    chore: remove Sentry tracking

--- src/custom/pages/App/index.tsx
+++ src/custom/pages/App/index.tsx
@@ -14,30 +14,9 @@ import NotFound from 'pages/error/NotFound'
 import CowRunner from 'pages/games/CowRunner'
 import MevSlicer from 'pages/games/MevSlicer'
 import AnySwapAffectedUsers from 'pages/error/AnySwapAffectedUsers'
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
-import { version } from '@src/../package.json'
-import { environmentName } from 'utils/environments'
 import { useFilterEmptyQueryParams } from 'hooks/useFilterEmptyQueryParams'
 import RedirectAnySwapAffectedUsers from 'pages/error/AnySwapAffectedUsers/RedirectAnySwapAffectedUsers'
 import { IS_CLAIMING_ENABLED } from 'pages/Claim/const'
-
-const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN
-const SENTRY_TRACES_SAMPLE_RATE = process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE
-
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    release: 'CowSwap@v' + version,
-    environment: environmentName,
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE ? Number(SENTRY_TRACES_SAMPLE_RATE) : 1.0,
-  })
-}
 
 export const Wrapper = styled(AppMod)``
 
